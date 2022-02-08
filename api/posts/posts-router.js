a86dbcc78783e892a8730e25169c1e198aa55de3
Sharon Sanchez <sstharakan@gmail.com>
2022-02-08 16:41:36 -0600

    able to [GET] /api/posts and passing the first test group

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,44 @@
 // implement your posts router here
 const express = require ('express')
 
+// data access function 
+const Post = require('./posts-model')
+
 const router = express.Router()
+
+// start the endpoints 
+
+router.get('/', (req, res) => {
+    Post.find()
+    .then(found => {
+        res.json(found)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            err: err.message,
+            stack:err.stack,
+        })
+    })
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+
+})
+router.put('/:id', (req, res) => {
+
+})
+router.get('/:id/messages', (req, res) => {
+
+})
+
 
 module.exports = router 
--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const server = express()
 
 server.use(express.json())
 
-server.use('api/posts', postsRouter)
+server.use('/api/posts', postsRouter)
 
 
 server.use('*', (req, res) => {
