4e1c13bd9ef2c578c31971a766d5e06d94331ccc
Sharon Sanchez <sstharakan@gmail.com>
2022-02-08 20:58:00 -0600

    able to get all the comments associated to the posts with proven id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -132,8 +132,24 @@ router.delete('/:id', async (req, res) => {
 
 // #### 6 [GET] /api/posts/:id/comments
 
-router.get('/:id/messages', (req, res) => {
-
+router.get('/:id/comments', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id)
+        if(!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        } else {
+            const messages = await Post.findPostComments(req.params.id)
+            res.json(messages)
+        }
+    } catch(err){
+        res.status(500).json({
+            message: "The comments information could not be retrieved",
+            err:err.message,
+            stack: err.stack,
+        })
+    }
 })
 
 
